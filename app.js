a0a19ab400d472d82301baa7efae2316f2558a56
Fixxoo <rajpalmal40@gmail.com>
2022-10-13 16:44:11 +0530

    added featuress

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const textarea = document.getElementById("textarea");
 const clearBTN = document.getElementById("clear");
 const lowerBTN = document.getElementById('LowerBtn');
 const upperBTN = document.getElementById('upperBtn');
-const sentenseBTN = document.getElementById('sentenceBtn');
+const sentenceBTN = document.getElementById('sentenceBtn');
+const titleBTN = document.getElementById('titleBtn');
 const copyBTN = document.getElementById('copyBtn');
 
 textarea.addEventListener("keyup", () => {
@@ -60,18 +61,37 @@ upperBTN.addEventListener('click', () => {
     textarea.value = textarea.value.toUpperCase();
 })
 
-//  sectenceCASE
-sentenseBTN.addEventListener('click', () => {
-    sentenceCASE();
+//  TitleCASE
+titleBTN.addEventListener('click', () => {
+   
+
+    titlecase(textarea.value);
+
+
+
+//     var text = textarea.value.toLowerCase().split(" ").map( (elem) => {
+//        return elem[0].toUpperCase() + elem.slice(1);
+// })
+//      var formated =  text.join(" ")
+//  console.log(formated)
+//  textarea.value = formated;
+
 })
-function sentenceCASE() {
-    console.log(textarea.value.charAt(0))
-    var split = textarea.value.toLowerCase().split("");
-    // console.log( s;
-    for (var i = 0; i < split.length; i++) {
-        // textarea.value = split.charAt(0).toUpperCase()+ split;
+
+// TODO
+function titlecase(textt){
+    var text = textt.toLowerCase().split(' ');
+//  console.log(textarea.value.split('\n'))
+
+    // console.log(text)
+    for(var i=0;i<text.length;i++){
+        // text.split('\n')
+      text[i]  = text[i][0].toUpperCase() + text[i].slice(1);
     }
+    textarea.value = text.join(' ')
 }
+
+
 
 copyBTN.addEventListener('click', () => {
     // Select the text field
@@ -82,5 +102,28 @@ copyBTN.addEventListener('click', () => {
     navigator.clipboard.writeText(textarea.value);
 })
 
+// TODO
+sentenceBTN.addEventListener('click', ()=>{
+    SentenceCASE();
+})
+// TODO BUG
+ function  SentenceCASE(){
+     var text = textarea.value;
+     console.log(text[16])
+    var newSTR ='';
+    for(i =0;i<text.length;i++){
+        if(i == 0){
+            newSTR += text[i].toUpperCase();
+        }
+        if(text[i] == '.'){
+            newSTR +="." + text[i+1].toUpperCase();
+        }
+        if(text[i] == '.' && text[i+1] ==" "){
+            newSTR +=  text[i+2].toUpperCase();
+        }
+        else newSTR += text[i].toLowerCase();
+    }
+    textarea.value = newSTR;
+ }
 
-
+ 
